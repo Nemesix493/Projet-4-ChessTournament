62ccc6843db51db518ca343c7eb2616530951695
Nemesix493 <verquerre.kevin@gmail.com>
2022-08-23 17:43:07 +0200

    change Models module:     change Player class:         add (overload) field_dict classproperty to list the model property         add (overload) __init__ method to unjson birthdate         add (overload) serialize method to json birthdate         add (overload) check_field_value method to check that rank is positive     change Tournament class:         add (overload) field_dict classproperty to list the model property         add (overload) __init__ method to unjson date, rounds and players and to load the players and rounds         add (overload) serialize method to json date, rounds and players         add (overload) check_field_value method to check that date is a list of datetime date, that players is a list of Players and that rounds is a list of Rounds

--- Models.py
+++ Models.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+
 from database import TinyDBDataBase
 
 
@@ -100,13 +103,113 @@ class Model:
         return [cls(**document) for document in doc_list]
 
 
+class Match(Model):
+    pass
+
+
 class Round(Model):
     pass
 
 
 class Player(Model):
-    pass
+    field_dict = {
+        'last_name': {
+            'type': str
+        },
+        'first_name': {
+            'type': str
+        },
+        'birthdate': {
+            'type': datetime.date
+        },
+        'gender': {
+            'type': str
+        },
+        'rank': {
+            'type': int
+        }
+    }
+
+    def __init__(self, **kwargs):
+        super(Player, self).__init__(**kwargs)
+        if 'birthdate' in kwargs.keys():
+            self.birthdate = json.loads(kwargs['birthdate'])
+
+    def serialize(self) -> dict:
+        result = super(Player, self).serialize()
+        result['birthdate'] = json.dumps(result['birthdate'])
+        return result
+
+    def check_field_value(self, name, value) -> bool:
+        super(Player, self).check_field_value(name=name, value=value)
+        if name == 'rank':
+            if value < 0:
+                raise ValueError(f'rank property has to be positive but it is {value}')
+        return True
 
 
 class Tournament(Model):
-    pass
+    field_dict = {
+        'name': {
+            'type': str,
+        },
+        'place': {
+            'type': str
+        },
+        'date': {
+            'type': list
+        },
+        'round_number': {
+            'type': int,
+            'default': 4
+        },
+        'description': {
+            'type': str
+        },
+        'time_control': {
+            'type': str
+        },
+        'rounds': {
+            'type': list
+        },
+        'players': {
+            'type': list
+        }
+    }
+
+    def __init__(self, **kwargs):
+        super(Tournament, self).__init__(**kwargs)
+        if 'date' in kwargs.keys():
+            self.date = json.loads(kwargs['date'])
+        if self.pk:
+            players = json.loads(kwargs['players'])
+            self.players = [Player.get(key='pk', value=player) for player in players]
+            rounds = json.loads(kwargs['rounds'])
+            self.players = [Round.get(key='pk', value=tournament_round) for tournament_round in rounds]
+
+    def check_field_value(self, name, value) -> bool:
+        super(Tournament, self).check_field_value(name=name, value=value)
+        if name == 'rounds':
+            for tournament_round in value:
+                if type(tournament_round) != Round:
+                    raise TypeError(
+                        f'All items in rounds property has to be {Round} but one is {type(tournament_round)}'
+                    )
+        if name == 'players':
+            for player in value:
+                if type(player) != Player:
+                    raise TypeError(f'All items in players property has to be {Player} but one is {type(player)}')
+        if name == 'date':
+            for date in value:
+                if type(date) != datetime.date:
+                    raise TypeError(
+                        f'All items in date property has to be {datetime.date} but one is {type(date)}'
+                    )
+        return True
+
+    def serialize(self) -> dict:
+        result = super(Tournament, self).serialize()
+        result['date'] = json.dumps(result['date'])
+        result['players'] = json.dumps([player.pk for player in result['players']])
+        result['rounds'] = json.dumps([tournament_round.pk for tournament_round in result['rounds']])
+        return result
