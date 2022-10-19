ce2db033484ab2ecf0274a2f0f0c68606de7c538
Nemesix493 <verquerre.kevin@gmail.com>
2022-10-19 17:06:32 +0200

    change Controllers package:     add Player module:         add PlayerController class:             add normalize_name method check the validity of the name             add new_player method manage the manu to add a new player             add list_player method manage the menu to display a player list to choose one             add display_player method not finished just a test version

---
+++ Contollers/Player.py
@@ -0,0 +1,123 @@
+import datetime
+
+from Views.Main import ViewsInterface
+import Models
+
+
+class PlayerController:
+    def normalize_name(self, name) -> str:
+        if not name.replace(' ', '').replace('-', '').isalpha():
+            raise ValueError('contient des caractères spéciaux autre que "-" ou des chiffres !')
+        normed_list = [[word.capitalize() for word in words.split('-')]
+                       for words in name.split(' ')]
+        normed_name = ' '.join(['-'.join(words) for words in normed_list])
+        return normed_name
+
+    def new_player(self, view: ViewsInterface) -> None | Models.Player:
+        options = {
+            'last_name': 'Nom: ',
+            'first_name': 'Prénom: ',
+            'birthdate': 'Date de naissance (JJ/MM/YYYY): ',
+            'gender': 'Genre (M/F): ',
+            'rank': 'Rang: '
+        }
+        player_dict = view.form(
+            title="Ajout d'un nouveau joueur",
+            items=options
+        )
+        while True:
+            player = Models.Player()
+            no_error = True
+            error = None
+            while True:
+                try:
+                    player_dict['last_name'] = self.normalize_name(player_dict['last_name'])
+                    break
+                except ValueError as e:
+                    player_dict['last_name'] = view.form(
+                        header=f'Nom invalide !\n{player_dict["last_name"]} {e}',
+                        fields={'last_name': 'Nom: '}
+                    )
+            while True:
+                try:
+                    player_dict['first_name'] = self.normalize_name(player_dict['first_name'])
+                    break
+                except ValueError as e:
+                    player_dict['first_name'] = view.form(
+                        header=f'Prénom invalide !\n"{player_dict["first_name"]}" {e}',
+                        fields={'first_name': 'Prénom: '}
+                    )
+            if no_error:
+                try:
+                    split_date = [int(val) for val in player_dict['birthdate'].split('/')]
+                    player.birthdate = datetime.date(
+                        year=split_date[2],
+                        month=split_date[1],
+                        day=split_date[0]
+                    )
+                    if not player.birthdate <= datetime.date.today():
+                        raise ValueError()
+                except ValueError:
+                    no_error = False
+                    error = f'"{options["birthdate"]} {player_dict["birthdate"]}" est invalide !'
+            player_dict['gender'] = player_dict['gender'].replace(' ', '')
+            if no_error and (player_dict['gender'] == 'M' or player_dict['gender'] == 'F'):
+                player.gender = player_dict['gender']
+            else:
+                no_error = False
+                error = f'"{options["gender"]} {player_dict["gender"]}" est invalide !'
+            if no_error:
+                try:
+                    player.rank = int(player_dict['rank'])
+                except ValueError:
+                    no_error = False
+                    error = f'"{options["rank"]} {player_dict["rank"]}" est invalide !'
+            if no_error:
+                break
+            else:
+                player_dict = view.form(
+                    header=error,
+                    items=options
+                )
+        while True:
+            result = view.menu(
+                header='',
+                items=[
+                    'Enregistrer le nouveau joueur',
+                    'Ne pas enregistrer'
+                ]
+            )
+            if result.replace(' ', '') == '1':
+                player.save()
+                return player
+            elif result.replace(' ', '') == '2':
+                return None
+
+    def list_player(self, view: ViewsInterface):
+        all_player = Models.Player.get_all(key=None, value=None)
+        header = f'Choisissez un joueur pour avoir plus d\'information'
+        items_list =[
+            *[f'{player.first_name} {player.last_name}' for player in all_player],
+            'Retour au menu principal'
+        ]
+        option_key = view.menu(
+            items=items_list,
+            header=header,
+            title='Liste des Joueurs'
+        )
+        while True:
+            while not (option_key.isnumeric() and option_key != '0'):
+                option_key = view.menu(header='Option invalide !', items=[])
+            option_key = int(option_key)
+            if option_key >= len(items_list):
+                return None
+            self.display_player(player=all_player[option_key - 1])
+            option_key = view.menu(items=['Continuer', 'Retour au menu principal'])
+            while not (option_key.isnumeric() and option_key != '0'):
+                option_key = view.menu(header='Option invalide !', items=[])
+            option_key = int(option_key)
+            if option_key >= 2:
+                return None
+
+    def display_player(self, player: Models.Player):
+        print(player.first_name)
