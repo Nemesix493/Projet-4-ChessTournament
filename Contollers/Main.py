d197d65ec4c81fa7c60c58dd17623f44b4d03342
Nemesix493 <verquerre.kevin@gmail.com>
2022-10-26 18:08:03 +0200

    change Controller package:     change Main module:         change main_menu function to main_controller and simplify it by using checker.check_menu

--- Contollers/Main.py
+++ Contollers/Main.py
@@ -1,9 +1,15 @@
 from Views.Main import ViewsInterface
 from .Player import PlayerController
 from .Tournament import TournamentController
+from .Checkers import check_menu
 
 
-def main_menu(view: ViewsInterface):
+def main_controller(view: ViewsInterface) -> None:
+    """
+    Display the main menu, and manage the execution of submenus
+    :param view: ViewsInterface
+    :return: None
+    """
     options = [
         ('Nouveau tournoi', TournamentController().new_tournament, {'view': view}),
         ('Nouveau joueur', PlayerController().new_player, {'view': view}),
@@ -11,18 +17,11 @@ def main_menu(view: ViewsInterface):
         ('Liste des tournois', TournamentController().list_tournament, {'view': view}),
         ('Quitter', quit, {})
     ]
-    option_key = view.menu(title="Menu principal", items=[option[0] for option in options])
     while True:
-        option_key = option_key.replace(' ', '')
-        try:
-            option_key = int(option_key) - 1
-            if option_key in range(len(options)):
-                options[option_key][1](**options[option_key][2])
-                option_key = view.menu(title="Menu principal", items=[option[0] for option in options])
-            else:
-                raise ValueError()
-        except ValueError:
-            option_key = view.menu(
-                header='Option invalide veuiller rentrer une option valide !',
-                items=[option[0] for option in options]
-            )
+        option_index = check_menu(
+            view=view,
+            items=[option[0] for option in options],
+            title='Menu principal',
+            invalid_header='Option invalide veuiller rentrer une option valide !'
+        )
+        options[option_index][1](**options[option_index][2])
