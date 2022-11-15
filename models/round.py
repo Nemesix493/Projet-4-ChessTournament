00b229e489ceb423d2d24f69f808f90279920487
Nemesix493 <verquerre.kevin@gmail.com>
2022-11-15 23:04:02 +0100

    adding missing fields in round class and fix constructor issues for the data loading from db

--- models/round.py
+++ models/round.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 
@@ -7,23 +8,34 @@ from .player import Player
 
 class Round(Model):
     field_dict = {
+        'name': {
+            'type': str
+        },
+        'start': {
+            'type': datetime.datetime
+        },
+        'end': {
+            'type': datetime.datetime
+        },
         'matches': {
             'type': list
         }
     }
 
     def __init__(self, **kwargs):
-        super(Round, self).__init__(**kwargs)
-        if self.pk:
-            matches = json.loads(kwargs['matches'])
-            self.matches = [
-                {
-                    'player_1': Player.get(key='pk', value=match['player_1']),
-                    'player_2': Player.get(key='pk', value=match['player_2']),
-                    'winner': Player.get(key='pk', value=match['winner'])
-                }
+        self_value_dict = {key: val for key, val in kwargs.items()}
+        if 'pk' in self_value_dict.keys():
+            matches = json.loads(self_value_dict['matches'])
+            self_value_dict['matches'] = [
+                (
+                    [Player.get(key='pk', value=match[0][0]), match[0][1]],
+                    [Player.get(key='pk', value=match[1][0]), match[1][1]]
+                )
                 for match in matches
             ]
+            self_value_dict['start'] = datetime.datetime.fromtimestamp(self_value_dict['start'])
+            self_value_dict['end'] = datetime.datetime.fromtimestamp(self_value_dict['end'])
+        super(Round, self).__init__(**self_value_dict)
 
     def serialize(self) -> dict:
         """
@@ -33,14 +45,15 @@ class Round(Model):
         result = super(Round, self).serialize()
         result['matches'] = json.dumps(
             [
-                {
-                    'player_1': match['player_1'].pk,
-                    'player_2': match['player_2'].pk,
-                    'winner': match['winner'].pk
-                }
+                (
+                    [match[0][0].pk, match[0][1]],
+                    [match[1][0].pk, match[1][1]]
+                )
                 for match in result['matches']
             ]
         )
+        result['start'] = result['start'].timestamp()
+        result['end'] = result['end'].timestamp()
         return result
 
     def check_field_value(self, name, value) -> bool:
@@ -53,15 +66,13 @@ class Round(Model):
         super(Round, self).check_field_value(name=name, value=value)
         if name == 'matches':
             for match in value:
-                if type(match) == dict:
-                    keys = ['player_1', 'player_2', 'winner']
-                    if False not in [key in match.keys() for key in keys]:
-                        if False in [type(match[key]) == Player for key in keys]:
-                            raise TypeError(f'One item of the a dict in the list is not a {Player} but must be one !')
-                    else:
-                        raise ValueError(
-                            f'Missing a key in a dict of the list all dict in the list must have {keys} keys'
-                        )
+                if type(match) == tuple:
+                    for line in match:
+                        if type(line[0]) != Player:
+                            raise TypeError(f'The player must be a Player object but is a {type(line[0])}')
+                        if type(line[1]) != int and type(line[1]) != float:
+                            raise TypeError(f'The score must be a int or float but is a {type(line[1])}')
                 else:
-                    raise TypeError(f'All item of the matches list must be a dict but one is a {type(match)}')
+                    raise TypeError(f'All item of the matches list must be a tuple but one is a {type(match)}')
         return True
+
